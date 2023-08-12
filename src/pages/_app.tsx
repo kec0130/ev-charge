433f3b46d49741159ac4126716f91e68feb8ea02
eunchae <kec0130@gmail.com>
2023-08-12 23:11:20 +0900

    Add: favicon and icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+
 import customTheme from '@/styles/customTheme';
 import Layout from '@/components/Layout';
 
@@ -7,6 +9,16 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={customTheme}>
       <Layout>
+        <Head>
+          <meta name='viewport' content='width=device-width, initial-scale=1' />
+          <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+          <link rel='manifest' href='/site.webmanifest' />
+          <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#55cc88' />
+          <meta name='msapplication-TileColor' content='#2b5797' />
+          <meta name='theme-color' content='#ffffff' />
+        </Head>
         <Component {...pageProps} />
       </Layout>
     </ChakraProvider>
