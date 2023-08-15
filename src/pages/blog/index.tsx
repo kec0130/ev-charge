8026311c00247740f21073beff9cc421680c5c36
eunchae <kec0130@gmail.com>
2023-08-15 16:33:38 +0900

    Add: introduction post

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -14,6 +14,45 @@ const Blog = () => {
       />
       <List>
         <ListItem mb={12}>
+          <Link href='/blog/ev-charge-introduction'>
+            <Flex w='full' flexDir={['column', 'row']}>
+              <Box
+                mr={[0, 8]}
+                mb={[4, 0]}
+                w='full'
+                maxW={['full', 300]}
+                borderRadius='md'
+                overflow='hidden'
+              >
+                <Image
+                  src='/images/common/og.png'
+                  alt='전기차G'
+                  width={300}
+                  height={158}
+                  style={{
+                    objectFit: 'cover',
+                    width: '100%',
+                    height: '100%',
+                  }}
+                />
+              </Box>
+              <Box>
+                <Heading as='h3' size='lg' mb={[2, 4]} noOfLines={[2, 1]}>
+                  전기차충전소 찾기, 실시간 조회 서비스 &apos;전기차G&apos;
+                </Heading>
+                <Text noOfLines={[3, 4]}>
+                  전기차충전소 찾기 서비스 전기차G를 소개합니다. 전국 전기차충전소를 쉽고 빠르게
+                  조회하실 수 있습니다. 충전소 위치, 영업시간, 가능여부 등을 실시간으로 정보를
+                  전달해드립니다. 위치 파악을 통해 검색 없이도 빠르게 주변 충전소를 찾아보실 수 있는
+                  서비스입니다. 충전소 이외 전기차 관련 부가서비스 정보제공을 지속 업데이트 할
+                  예정입니다.
+                </Text>
+              </Box>
+            </Flex>
+          </Link>
+        </ListItem>
+
+        <ListItem mb={12}>
           <Link href='/blog/electric-car-subsidy'>
             <Flex w='full' flexDir={['column', 'row']}>
               <Box
