d35c39244dd7995d095f71a6292e3cfd6cd87a53
eunchae <kec0130@gmail.com>
2023-08-18 21:46:12 +0900

    Add: google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,6 +16,18 @@ export default function Document() {
           crossOrigin='anonymous'
           strategy='afterInteractive'
         />
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+          strategy='afterInteractive'
+        />
+        <Script id='google-analytics' strategy='afterInteractive'>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
+          `}
+        </Script>
       </Head>
       <body>
         <Main />
