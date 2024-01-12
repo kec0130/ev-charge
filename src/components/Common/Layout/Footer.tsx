5cb212e4627d41d43ccb3158469ae962825dd9d5
eunchae <kec0130@gmail.com>
2024-01-12 19:55:26 +0900

    Update footer

--- src/components/Common/Layout/Footer.tsx
+++ src/components/Common/Layout/Footer.tsx
@@ -1,25 +1,49 @@
-import { Box, Container, Divider, Text } from '@chakra-ui/react';
+import Image from 'next/image';
+import { Box, Container, Divider, Flex, Link, Text } from '@chakra-ui/react';
 
 const Footer = () => {
   return (
-    <Container
-      as='footer'
-      maxW='container.lg'
-      textAlign='center'
-      mb='env(safe-area-inset-bottom)'
-      pb={4}
-    >
-      <Divider mb={4} />
-      <Box color='gray.400' fontSize='sm' lineHeight={6}>
-        <Text>문의: chaechae.couple@gmail.com</Text>
-        <Text>&copy; 전기차G | 전기차 충전소 찾기</Text>
-        <Text mt={2} fontWeight='bold' textTransform='uppercase'>
-          Family Site
-        </Text>
-        <a href='https://messagebot.chaechae.life' target='_blank' rel='noreferrer'>
-          메시지봇 - 인사말 생성 AI 챗봇
-        </a>
-      </Box>
+    <Container as='footer' maxW='container.lg' pb='env(safe-area-inset-bottom)'>
+      <Divider />
+      <Flex
+        fontSize='sm'
+        lineHeight={6}
+        px={[0, 2]}
+        py={6}
+        justifyContent='space-between'
+        flexDir={['column', 'row']}
+        gap={4}
+        color='gray.600'
+      >
+        <Box>
+          <Image src='/logo.png' alt='전기차 충전소 찾기' width={80} height={34} />
+          <Text>&copy; {new Date().getFullYear()} EV Charge. All Rights Reserved.</Text>
+        </Box>
+
+        <Flex
+          flexDir={['column', 'column', 'row']}
+          gap={4}
+          justifyContent='space-between'
+          w={['full', '40%']}
+        >
+          <Flex flexDir='column'>
+            <Text fontWeight='bold' textTransform='uppercase'>
+              About Us
+            </Text>
+            <Link href='https://chaechae.life' target='_blank' rel='noreferrer noopener'>
+              개발자 소개
+            </Link>
+          </Flex>
+          <Flex flexDir='column'>
+            <Text fontWeight='bold' textTransform='uppercase'>
+              Family Site
+            </Text>
+            <Link href='https://messagebot.chaechae.life' target='_blank' rel='noreferrer noopener'>
+              메시지봇 - 인사말 생성 AI 챗봇
+            </Link>
+          </Flex>
+        </Flex>
+      </Flex>
     </Container>
   );
 };
