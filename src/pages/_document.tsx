aaf03c2fdbc493b96a89a484799ac59d206e1dff
eunchae <kec0130@gmail.com>
2024-01-27 11:50:40 +0900

    Redeploy

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,12 +10,14 @@ export default function Document() {
           type='text/javascript'
           src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NCP_CLIENT_ID}&submodules=geocoder`}
         />
+        {/* google adsense */}
         <Script
           async
           src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
           crossOrigin='anonymous'
           strategy='afterInteractive'
         />
+        {/* google analytics */}
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
           strategy='afterInteractive'
