bf3d75f2b8c400a86cc90eaec8e728eea7fe58bf
eunchae <kec0130@gmail.com>
2024-01-31 18:18:23 +0900

    Add blog infeed ads

--- src/components/Blog/PostList.tsx
+++ src/components/Blog/PostList.tsx
@@ -1,19 +1,28 @@
-import { List } from '@chakra-ui/react';
+import { Divider, List } from '@chakra-ui/react';
 import { Post } from 'contentlayer/generated';
 import PostListItem from './PostListItem';
+import BlogInfeedDesktopAds from '../Common/AdSense/BlogInfeedDesktopAds';
 
 const PostList = ({ posts }: { posts: Post[] }) => {
   return (
     <List>
       {posts.map((post, index) => (
-        <PostListItem
-          key={post.slug}
-          title={post.title}
-          description={post.description}
-          slug={post.slug}
-          imgSrc={post.image_url}
-          index={index}
-        />
+        <>
+          <PostListItem
+            key={post.slug}
+            title={post.title}
+            description={post.description}
+            slug={post.slug}
+            imgSrc={post.image_url}
+            index={index}
+          />
+          {index !== 0 && index !== posts.length - 1 && index % 4 === 0 && (
+            <>
+              <Divider my={6} />
+              <BlogInfeedDesktopAds />
+            </>
+          )}
+        </>
       ))}
     </List>
   );
